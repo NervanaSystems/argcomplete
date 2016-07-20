4d9ad51e575e9fa3c414d7656220db6b9c6ec58e
Igor Kaplounenko <igor@nervanasys.com>
2016-07-20 01:45:10 +0000

    added __init__

--- argcomplete/__init__.py
+++ argcomplete/__init__.py
@@ -4,7 +4,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import os, sys, contextlib
-import ncloud.vendor.python.argparse
+from ncloud.vendor.python import argparse
 from . import completers, my_shlex as shlex
 from .compat import USING_PYTHON2, str, sys_encoding, ensure_str, ensure_bytes
 from .completers import FilesCompleter
